fd83edbe9c71d49e4c560ab4bdd0d0adf6ff5f6f
writerror <writerror@users.noreply.github.com>
2017-04-23 15:57:42 -0400

    +feed

--- assets/main.js
+++ assets/main.js
@@ -36,6 +36,7 @@ $(function() {
         //{ title: 'Rabble', url: 'http://rabble.org.uk/feed/' },
         //{ title: 'Mpalothia', url: 'http://mpalothia.net/category/english/feed/' },
         { title: 'Elephant Archives', url: 'https://archive.elephanteditions.net/feed' },
+        { title: 'Modern Slavery', url: 'http://modernslavery.calpress.org/?feed=rss2' },
     ];
 
 	$container.before('<div class="text-center"><div id="loader" class="clearfix">Loading feeds&hellip;</div><select id="siteSort" class="hidden"><option value="*" selected="selected">All Sources</option></select></div>');
