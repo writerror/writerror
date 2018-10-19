a55d3d47ec4e7ac99c5054ed746b2fe03fce7b4f
writerror <writerror@users.noreply.github.com>
2018-10-18 22:16:26 -0400

    -feed

--- assets/main.js
+++ assets/main.js
@@ -27,7 +27,6 @@ $(function() {
         { title: 'Anarchist News', url: 'https://anarchistnews.org/rss.xml' },
         { title: 'The Final Straw Radio', url: 'https://thefinalstrawradio.noblogs.org/feed/' },
 		{ title: 'Attaque', url: 'https://attaque.noblogs.org/post/category/english/feed/' },
-		{ title: 'mpalothia', url: 'https://mpalothia.net/category/english/feed/' },
     ];
 
 	$container.before('<div class="text-center"><div id="loader" class="clearfix">Loading feeds&hellip;</div><select id="siteSort" class="hidden"><option value="*" selected="selected">All Sources</option></select></div>');
